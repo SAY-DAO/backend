2fb6715f882cb087e615247943c5192314fb97b0
MosTafa SM <mostafa.sot@mtnirancell.ir>
2019-07-17 11:31:24 +0430

    'db'

--- say/api/__init__.py
+++ say/api/__init__.py
@@ -9,7 +9,7 @@ from flasgger import Swagger
 from flasgger.utils import swag_from
 from werkzeug.utils import secure_filename
 
-db = create_engine('postgresql://postgres:postgres@localhost:5432/say')
+db = create_engine('postgresql://postgres:postgres@postgres:5432/say')
 
 UPLOAD_FOLDER = "C:\\Users\\Parsa\\PycharmProjects\\SAY\\say\\files"
 FLAGS = "C:\\Users\\Parsa\\PycharmProjects\\SAY\\say\\flags"
