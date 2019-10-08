861fadd7493b7ea7f44a389dc04d1e87740e0e52
Arashfatahzade <fatahzade@gmail.com>
2019-10-08 18:46:22 +0330

    Changed panel login parameter to form

--- say/api/panel_auth_api.py
+++ say/api/panel_auth_api.py
@@ -15,48 +15,6 @@ Panel Authentication APIs
 """
 
 
-class CheckUser(Resource):
-    def post(self):
-        session_maker = sessionmaker(db)
-        session = session_maker()
-        resp = {"message": "major error occurred!"}
-
-        try:
-            if "username" in request.json.keys():
-                username = request.json["username"]
-            else:
-                return Response(
-                    json.dumps({"message": "username is needed !!!"}), status=500
-                )
-
-            alreadyTaken = (
-                session.query(UserModel)
-                .filter_by(isDeleted=False)
-                .filter_by(userName=username)
-                .first()
-            )
-            if alreadyTaken is not None:
-                resp = Response(
-                    json.dumps(
-                        {"status": False, "Message": "UserName is Already Taken"}
-                    ),
-                    status=200,
-                )
-            else:
-                resp = Response(
-                    json.dumps({"status": True, "Message": "UserName is Acceptable"}),
-                    status=200,
-                )
-
-        except Exception as e:
-            print(e)
-            resp = Response(json.dumps({"message": "ERROR OCCURRED"}))
-
-        finally:
-            session.close()
-            return resp
-
-
 class PanelLogin(Resource):
 
     @swag_from("./docs/panel_auth/login.yml")
@@ -66,16 +24,17 @@ class PanelLogin(Resource):
         resp = {"message": "Something is Wrong!"}
 
         try:
+            from pudb import set_trace; set_trace()
 
-            if "username" in request.json.keys():
-                username = request.json["username"]
+            if "username" in request.form.keys():
+                username = request.form["username"]
             else:
                 return Response(
                     json.dumps({"message": "username is needed !!!"}), status=500
                 )
 
-            if "password" in request.json.keys():
-                password = md5(request.json["password"].encode()).hexdigest()
+            if "password" in request.form.keys():
+                password = md5(request.form["password"].encode()).hexdigest()
             else:
                 return Response(
                     json.dumps({"message": "password is needed !!!"}), status=500
@@ -140,32 +99,6 @@ class PanelTokenRefresh(Resource):
         return {'access_token': f'Bearer {access_token}'}
 
 
-#class Logout(Resource):
-#
-#    @jwt_required
-#    def post(self, user_id):
-#        session_maker = sessionmaker(db)
-#        session = session_maker()
-#        resp = {"message": "fuck"}
-#
-#        try:
-#            user = session.query(UserModel).filter_by(id=user_id).first()
-#            user.token = ""
-#            session.commit()
-#            resp = Response(
-#                json.dumps({"message": "user is successfully logged out."}), status=200
-#            )
-#
-#        except Exception as e:
-#            print(e)
-#            resp = Response(json.dumps({"message": "Something is Wrong!"}), status=500)
-#
-#        finally:
-#            session.close()
-#            return resp
-#
-#
-#
 class PanelLogoutAccess(Resource):
     @jwt_required
     @swag_from("./docs/panel_auth/logout-access.yml")
@@ -203,89 +136,6 @@ class PanelLogoutRefresh(Resource):
         finally:
             session.close()
             return msg
-
-#
-#
-#class Verify(Resource):
-#    decorators = [limiter.limit("5/minute")]
-#
-#    @swag_from("./docs/auth/verify.yml")
-#    def post(self, user_id):
-#        session_maker = sessionmaker(db)
-#        session = session_maker()
-#        resp = {"message": "Something is Wrong"}
-#
-#        try:
-#            user = session.query(UserModel).filter_by(id=user_id).first()
-#            if user.isVerified:
-#                resp = Response(
-#                    json.dumps({"message": "User is already verified."}), status=200
-#                )
-#                return
-#
-#            verify = session.query(VerifyModel).filter_by(id_user=user_id).first()
-#            if (
-#                verify is None
-#                or "verifyCode" not in request.json.keys()
-#                or verify.expire_at < datetime.utcnow()
-#                or verify.code != request.json["verifyCode"]
-#            ):
-#                resp = Response(
-#                    json.dumps({"message": "Something is Wrong!"}), status=500
-#                )
-#                return
-#
-#            user.isVerified = True
-#            session.commit()
-#            resp = Response(
-#                json.dumps({"message": "User successfully verified."}), status=200
-#            )
-#
-#        except Exception as e:
-#            print(e)
-#            resp = Response(json.dumps({"message": "Something is Wrong!"}), status=500)
-#
-#        finally:
-#            session.close()
-#            return resp
-#
-#
-#class VerifyResend(Resource):
-#    decorators = [limiter.limit("2/minute")]
-#
-#    @swag_from("./docs/auth/verify-resend.yml")
-#    def post(self, user_id):
-#        session_maker = sessionmaker(db)
-#        session = session_maker()
-#        resp = {"message": "Something is Wrong"}
-#
-#        try:
-#            user = session.query(UserModel).filter_by(id=user_id).first()
-#            if user.isVerified:
-#                resp = Response(
-#                    json.dumps({"message": "User is already verified."}), status=200
-#                )
-#                return
-#
-#            verify = session.query(VerifyModel).filter_by(id_user=user_id).first()
-#            if verify is None:
-#                verify = VerifyModel(user=user)
-#                session.add(verify)
-#            verify.code = randint(100000, 999999)
-#            verify.expireAt = datetime.utcnow() + timedelta(minutes=5)
-#
-#            session.commit()
-#            send_verify_email(user.emailAddress, verify.code)
-#            resp = Response(json.dumps({"message": "Verify Email Sent."}), status=200)
-#
-#        except Exception as e:
-#            print(e)
-#            resp = Response(json.dumps({"message": "Something is Wrong!"}), status=500)
-#
-#        finally:
-#            session.close()
-#            return resp
-#
 
 """
 API URLs
