fab25557edf7bc17942ad623013e584806b08941
Arashfatahzade <fatahzade@gmail.com>
2019-10-14 16:00:05 +0330

    Some improvment on payment

--- say/api/payment_api.py
+++ say/api/payment_api.py
@@ -1,41 +1,16 @@
 from datetime import datetime
 import random
 
-from wtforms import Form, IntegerField, StringField, DecimalField, validators
 from say.models.need_model import NeedModel
 from say.models.user_model import UserModel
 from say.models.child_need_model import ChildNeedModel
+from say.models.family_model import FamilyModel
+from say.models.need_family_model import NeedFamilyModel
+from say.models.user_family_model import UserFamilyModel
 from say.models.payment_model import PaymentModel
 from . import *
 
 
-#    needId = IntegerField('needId', [
-#        validators.DataRequired()
-#    ])
-#    userId = IntegerField('userId', [
-#        validators.DataRequired(message=('userId الزامیست!'))
-#    ])
-#    amount = DecimalField('amount', [
-#        validators.DataRequired(message=('amount الزامیست!'))
-#    ])
-#
-#    def validate_needId(self, needId):
-#        from pudb import set_trace; set_trace()
-#        session_maker = sessionmaker(db)
-#        session = session_maker()
-#        need = session.query(NeedModel).get(needId)
-#        session.close()
-#        if need is None:
-#            raise ValueError("Need Not Found")
-#
-#    def validate_userId(self, userId):
-#        session_maker = sessionmaker(db)
-#        session = session_maker()
-#        user = session.query(UserModel).get(userId)
-#        session.close()
-#        if user is None:
-#            raise ValueError("User Not Found")
-#
 def validate_amount(need, amount):
     amount = int(amount)
     need_unpaid = need.cost - need.paid
@@ -73,21 +48,52 @@ class Payment(Resource):
                 resp = {"message": "Need Not Found"}
                 return
 
+            if not need.isConfirmed:
+                resp = make_response(
+                    jsonify({"message": "error: need is not confirmed yet!"}),
+                    422,
+                )
+                return resp
+
             user = session.query(UserModel).get(userId)
             if user is None:
                 resp = {"message": "User Not Found"}
                 return
 
+            child_need = (
+                session.query(ChildNeedModel)
+                .filter_by(id_need=needId)
+                .first()
+            )
+
+            family = (
+                session.query(FamilyModel)
+                .filter_by(id_child=child_need.id_child)
+                .filter_by(isDeleted=False)
+                .first()
+            )
+
+            if (
+                session.query(UserFamilyModel)
+                .filter_by(isDeleted=False)
+                .filter_by(id_family=family.id)
+                .filter_by(id_user=userId)
+                .first()
+                is None
+            ):
+                resp = make_response(jsonify({"message": "payment must be added by the child's family!"}), 422)
+                return resp
+
+
             try:
                 amount = validate_amount(need, amount)
             except ValueError as e:
-                resp = {"message": str(e)}
+                resp = make_response(jsonify({"message": str(e)}), 422)
                 return
-
 
             order_id = str(user.id) \
                 + str(need.id) \
-                + str(random.randint(1 , 1000))
+                + str(random.randint(100 , 1000))
 
             api_data = {
                 "order_id": order_id,
@@ -112,6 +118,7 @@ class Payment(Resource):
                 desc=api_data['desc'],
             )
             session.add(new_payment)
+
             session.commit()
             resp = jsonify(obj_to_dict(new_payment))
             resp.headers.add("Access-Control-Allow-Origin", "*")
@@ -121,7 +128,7 @@ class Payment(Resource):
             )
 
         except Exception as ex:
-            resp = str(ex)
+            resp = make_response(str(ex), 422)
 
         finally:
             session.close()
@@ -135,10 +142,8 @@ class VerifyPayment(Resource):
 
         response = idpay.verify(paymentId, orderId)
         if response['status'] != 100:
-            resp = make_response(
-                jsonify(dict(message=idpay.RESPONSES[response['status']]))
-            )
-            return resp
+            #  TODO: what happens after unsusscesful payment
+            return redirect(app.config['BASE_URL'], 302)
 
         session_maker = sessionmaker(db)
         session = session_maker()
@@ -148,8 +153,11 @@ class VerifyPayment(Resource):
             .first()
 
         if pending_payment is None:
-            resp = dict(message='Invalid orderId')
-            return resp
+            resp = dict(message='Invalid Payment ID')
+            return make_response(resp, 422)
+
+        need = pending_payment.need
+        amount = pending_payment.amount
 
         pending_payment.is_verified = True
         pending_payment.date = datetime.fromtimestamp(int(
@@ -161,12 +169,53 @@ class VerifyPayment(Resource):
         ))
         pending_payment.card_no = response['payment']['card_no']
         pending_payment.hashed_card_no = response['payment']['hashed_card_no']
+
+        child_need = session.query(ChildNeedModel) \
+            .filter_by(id_need = pending_payment.need.id) \
+            .first()
+
+        child = child_need.child_relation
+        family = (
+            session.query(FamilyModel)
+            .filter_by(id_child=child.id)
+            .filter_by(isDeleted=False)
+            .first()
+        )
+
+        participant = (
+            session.query(NeedFamilyModel)
+            .filter_by(id_need=pending_payment.id_need)
+            .filter_by(id_user=pending_payment.id_user)
+            .filter_by(isDeleted=False)
+            .first()
+        )
+        if participant is None:
+            new_participant = NeedFamilyModel(
+                id_family=family.id,
+                id_user=pending_payment.id_user,
+                id_need=pending_payment.id_need,
+            )
+            session.add(new_participant)
+
+        need.paid += amount
+        need.progress = need.paid / need.cost * 100
+
+        if need.paid == need.cost:
+            need.isDone = True
+            # user.doneNeedCount += 1  # TODO: which one is correct?
+
+            participants = (
+                session.query(NeedFamilyModel)
+                .filter_by(id_need=need.id)
+                .filter_by(isDeleted=False)
+                .all()
+            )
+
+            for participate in participants:
+                participate.user_relation.doneNeedCount += 1
+
         session.commit()
 
-        child_id = session.query(ChildNeedModel) \
-            .filter_by(id_need = pending_payment.need.id) \
-            .first() \
-            .id_child
         resp = jsonify(obj_to_dict(pending_payment))
 
         return make_response(render_template(
