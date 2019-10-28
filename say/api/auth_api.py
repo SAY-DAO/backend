11288b6a65d089928b7f01a8231173ce4f5e2ab3
Arash Fatahzade <fatahzade@gmail.com>
2019-10-28 04:07:12 +0330

    Revert "done need count for child and spent credit for user fixed"
    This reverts commit 84c70985443ff88654637bf3d4148ec7eb891d2e

--- say/api/auth_api.py
+++ say/api/auth_api.py
@@ -36,7 +36,7 @@ class CheckUser(Resource):
 
         try:
             if "username" in request.json.keys():
-                username = request.json["username"].lower()
+                username = request.json["username"]
             else:
                 return Response(
                     json.dumps({"message": "userName is needed !!!"}), status=500
@@ -79,7 +79,7 @@ class RegisterUser(Resource):
         resp = {"message": "something is wrong"}
         try:
             if "username" in request.json.keys():
-                username = request.json["username"].lower()
+                username = request.json["username"]
             else:
                 resp = Response(
                     json.dumps({"message": "userName is needed !!!"}), status=500
@@ -204,7 +204,7 @@ class Login(Resource):
         try:
 
             if "username" in request.json.keys():
-                username = request.json["username"].lower()
+                username = request.json["username"]
             else:
                 resp = Response(
                     json.dumps({"message": "userName is needed !!!"}), status=500
