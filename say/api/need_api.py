9def0f686d55347d9a68017b87564338e610dd1b
parsa <parsaenami@gmail.com>
2019-10-29 21:06:44 +0330

    fixed participants roles

--- say/api/need_api.py
+++ say/api/need_api.py
@@ -45,17 +45,18 @@ def get_need(need, session, participants_only=False, with_participants=True, wit
 
     participant_ids = session.query(NeedFamilyModel).filter_by(id_need=need.id).filter_by(isDeleted=False).all()
     ids = [p.id_user for p in participant_ids]
-    
-    if len(participant_ids) > 0:
-        family = list(participant_ids)[0].id_family
 
     participants = (
         session.query(UserFamilyModel)
         .filter(UserFamilyModel.id_user.in_(ids))
-        .filter_by(id_family=family)
         .filter_by(isDeleted=False)
-        .all()
     )
+    
+    if len(participant_ids) > 0:
+        family = list(participant_ids)[0].id_family
+        participants = participants.filter_by(id_family=family)
+
+    participants = participants.all()
 
     users = {}
     for participant in participants:
