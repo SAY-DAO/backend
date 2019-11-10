2794d401ff87227d1a4d540a290a71c64d8b294d
Arashfatahzade <fatahzade@gmail.com>
2019-11-10 15:18:26 +0330

    Changed dates to datetime in payment

---
+++ alembic/versions/a9e54919491a_.py
@@ -0,0 +1,34 @@
+"""empty message
+
+Revision ID: a9e54919491a
+Revises: afbcf9baae46
+Create Date: 2019-11-10 15:15:11.311100
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a9e54919491a'
+down_revision = 'afbcf9baae46'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute('''
+        ALTER TABLE payment
+            ALTER COLUMN "date" TYPE TIMESTAMP WITH TIME ZONE;
+    ''')
+    op.execute('''
+        ALTER TABLE payment
+            ALTER COLUMN "createdAt" TYPE TIMESTAMP WITH TIME ZONE;
+    ''')
+    op.execute('''
+        ALTER TABLE payment
+            ALTER COLUMN "verfied_date" TYPE TIMESTAMP WITH TIME ZONE;
+    ''')
+
+def downgrade():
+    pass
--- say/models/payment_model.py
+++ say/models/payment_model.py
@@ -14,18 +14,18 @@ class PaymentModel(base):
     id_need = Column(Integer, ForeignKey(NeedModel.id), nullable=False)
     id_user = Column(Integer, ForeignKey(UserModel.id), nullable=False)
 
-    createdAt = Column(Date, nullable=False)
+    createdAt = Column(DateTime, nullable=False)
     orderId = Column(String, nullable=True)
     paymentId = Column(String, nullable=True)
     link = Column(String, nullable=True)
     amount = Column(Integer, nullable=True)
     desc = Column(String, nullable=True)
     is_verified = Column(Boolean, nullable=True)
-    date = Column(Date, nullable=True)
+    date = Column(DateTime, nullable=True)
     card_no = Column(String, nullable=True)
     hashed_card_no = Column(String, nullable=True)
     track_id = Column(String, nullable=True)
-    verfied_date = Column(Date, nullable=True)
+    verfied_date = Column(DateTime, nullable=True)
     donate = Column(Integer, default=0)
 
     need = relationship(
