e78dd15172f832238e3f4970eecf588f16de2670
Arashfatahzade <fatahzade@gmail.com>
2019-12-03 15:57:10 +0330

    Removed docker purne from scripts

--- scripts/deploy_nigthly.sh
+++ scripts/deploy_nigthly.sh
@@ -25,6 +25,6 @@ tar -xvf $CI_PROJECT_NAME_NIGTHLY.tar.gz &&
 cd $CI_PROJECT_NAME &&
 docker build -t $IMAGE_NAME . -f Dockerfile_nigthly &&
 cd /home/server/say-installer &&
-docker-compose up -d &&
+docker-compose up -d
 "
 echo 'DONE'
