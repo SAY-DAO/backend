abe7a9dccbfed71d26851214fb673f12501a8873
Arashfatahzade <fatahzade@gmail.com>
2019-12-07 17:53:57 +0330

    Resets timing to normal

--- say/celery.py
+++ say/celery.py
@@ -4,11 +4,11 @@ from celery.schedules import crontab
 beat = {
     'report-to-ngos': {
         'task': 'say.tasks.report_to_ngo.report_to_ngos',
-        'schedule': 3 * 60#crontab(minute=0, hour=23),
+        'schedule': crontab(minute=0, hour=23),
     },
     'update-needs': {
         'task': 'say.tasks.update_needs.update_needs',
-        'schedule': 60 * 60#crontab(minute=0, hour=23),
+        'schedule': 2 * 60 * 60#crontab(minute=0, hour=23),
     },
 }
 
