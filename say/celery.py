5bffda458d13801f3af5ca7c28c012b00f567be2
Arashfatahzade <fatahzade@gmail.com>
2019-12-09 18:21:22 +0330

    Revert "Changed timings"
    This reverts commit 6a450a69c8f0d3146be9d20b7883055262da8bd8.

--- say/celery.py
+++ say/celery.py
@@ -4,11 +4,11 @@ from celery.schedules import crontab
 beat = {
     'report-to-ngos': {
         'task': 'say.tasks.report_to_ngo.report_to_ngos',
-        'schedule': crontab(minute=0, hour=18),
+        'schedule': crontab(minute=0, hour=9),
     },
     'update-needs': {
         'task': 'say.tasks.update_needs.update_needs',
-        'schedule': crontab(minute=0, hour='6,8,20'),
+        'schedule': crontab(minute=0, hour='8,20'),
     },
 }
 
