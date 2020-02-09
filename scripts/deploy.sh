212bc2fd8d456ce5ea0e4198c688762ecf906c60
Arashfatahzade <fatahzade@gmail.com>
2020-02-10 01:53:20 +0330

    Fixed deploy-script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,8 +23,9 @@ cd /tmp &&
 tar -xvf $CI_PROJECT_NAME.tar.gz &&
 cd $CI_PROJECT_NAME &&
 docker build -t $IMAGE_NAME . &&
+cd .. &&
+rm -rf $CI_PROJECT_NAME $CI_PROJECT_NAME.tar.gz &&
 cd /home/server/say-installer &&
-docker-compose up -d &&
-rm -rf $CI_PROJECT_NAME $CI_PROJECT_NAME.tar.gz
+docker-compose up -d
 "
 echo 'DONE'
--- scripts/deploy_nigthly.sh
+++ scripts/deploy_nigthly.sh
@@ -24,9 +24,9 @@ cd $CI_PROJECT_DIR_NIGTHLY &&
 tar -xvf $CI_PROJECT_NAME_NIGTHLY.tar.gz &&
 cd $CI_PROJECT_NAME &&
 docker build -t $IMAGE_NAME . -f Dockerfile_nigthly &&
-cd /home/server/say-installer &&
-docker-compose up -d &&
-cd ../ &&
+cd .. &&
 rm -rf $CI_PROJECT_NAME $CI_PROJECT_NAME_NIGTHLY.tar.gz
+cd /home/server/say-installer &&
+docker-compose up -d
 "
 echo 'DONE'
