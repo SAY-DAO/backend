257e4320e1eed5be7d42a825d7f17e95f9334ca6
Arashfatahzade <fatahzade@gmail.com>
2020-02-18 23:40:46 +0330

    Removed timezone from need dates

---
+++ alembic/versions/1927cbecc60d_.py
@@ -0,0 +1,30 @@
+"""empty message
+
+Revision ID: 1927cbecc60d
+Revises: 5f88cc67b8d4
+Create Date: 2020-02-18 18:07:13.318806
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1927cbecc60d'
+down_revision = '5f88cc67b8d4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute('''
+        ALTER TABLE need ALTER COLUMN "doneAt" type timestamp without time zone;
+    ''')
+
+    op.execute('''
+        ALTER TABLE need ALTER COLUMN "confirmDate" type timestamp without time zone;
+    ''')
+
+
+def downgrade():
+    pass
