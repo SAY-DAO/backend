15db588cae5ce04e541b41891ce8de3f44cca98b
Arashfatahzade <fatahzade@gmail.com>
2020-02-19 00:00:49 +0330

    Added max_requests, max_requests_jitter and backlog to gunicorn

--- scripts/run-staging.sh
+++ scripts/run-staging.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 alembic upgrade head
-gunicorn -b 0.0.0.0:5000 --threads 1 --workers 2 run:app --reload
+gunicorn -b 0.0.0.0:5000 --threads 1 --workers 3 run:app --max-requests 100 -max_requests_jitter 42 --backlog 100
+
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 alembic upgrade head
-gunicorn -b 0.0.0.0:5000 --threads 2 --workers 4 run:app --reload
+gunicorn -b 0.0.0.0:5000 --threads 2 --workers 4 run:app --max-requests 100 -max_requests_jitter 42 --backlog 100
