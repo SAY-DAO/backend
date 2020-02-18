4ace4f0df71697bd85a545af12c77fb889ba9886
Arashfatahzade <fatahzade@gmail.com>
2020-02-19 00:08:55 +0330

    Fixed run.sh

--- scripts/run-staging.sh
+++ scripts/run-staging.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 alembic upgrade head
-gunicorn -b 0.0.0.0:5000 --threads 1 --workers 3 run:app --max-requests 100 --max_requests_jitter 42 --backlog 100
+gunicorn -b 0.0.0.0:5000 --threads 1 --workers 3 run:app --max-requests 100 --max-requests-jitter 132 --backlog 100
 
