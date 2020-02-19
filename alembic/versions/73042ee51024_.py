4bffc3ccf0c7ccd3aff00a19233e681e1a1fa8d4
parsa <parsa@say.company>
2020-02-19 16:22:13 +0330

    merged alembic heads

---
+++ alembic/versions/73042ee51024_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 73042ee51024
+Revises: 1927cbecc60d, 6298870eb1df
+Create Date: 2020-02-19 16:21:35.751173
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '73042ee51024'
+down_revision = ('1927cbecc60d', '6298870eb1df')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
