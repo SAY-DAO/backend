2f4003042dc5d78f3be635d1b7393cd9fd13beea
Arashfatahzade <fatahzade@gmail.com>
2020-04-13 16:47:40 +0430

    Revert "Changed report to sw temporary"
    This reverts commit 64140184f750e60447678cdb9b4d425672d8c49b.

--- say/celery.py
+++ say/celery.py
@@ -4,7 +4,7 @@ from celery.schedules import crontab
 beat = {
     'report-to-social-workers': {
         'task': 'say.tasks.report_to_social_worker.report_to_social_workers',
-        'schedule': 60#crontab(minute=30, hour=5),
+        'schedule': crontab(minute=30, hour=5),
     },
     'update-needs': {
         'task': 'say.tasks.update_needs.update_needs',
