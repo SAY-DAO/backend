2fc26587eb92aba101bbd1c8d8c8dc3b588cc13a
parsa <parsa@say.company>
2020-04-27 14:03:42 +0430

    db migrated

---
+++ alembic/versions/eba38b0d6f46_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: eba38b0d6f46
+Revises: f9a2e58b923c
+Create Date: 2020-04-26 21:41:39.831102
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eba38b0d6f46'
+down_revision = 'f9a2e58b923c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
