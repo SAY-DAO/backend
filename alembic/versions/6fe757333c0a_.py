531cffaab75d17db5aeae16d94e7b88ca6c17b15
Arashfatahzade <fatahzade@gmail.com>
2020-04-28 20:45:59 +0430

    Merged migrations

---
+++ alembic/versions/6fe757333c0a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6fe757333c0a
+Revises: 9aa35921bddc, eba38b0d6f46
+Create Date: 2020-04-28 18:30:15.751041
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6fe757333c0a'
+down_revision = ('9aa35921bddc', 'eba38b0d6f46')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
