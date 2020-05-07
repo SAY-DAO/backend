cfb6f0a1fe4dbd91e75057796021a76ce353dbc6
Arashfatahzade <fatahzade@gmail.com>
2020-05-07 22:11:37 +0430

    Implemented add-to-home notif

--- say/content/en.py
+++ say/content/en.py
@@ -1,6 +1,5 @@
 class ContentEN:
-    # TODO: pending content
     CONFIRM_PHONE = 'SAY verification code:\n %s'
     RESET_PASSWORD = 'Click on the link below to change your password.\n%s'
-    INSTALLION = 'en https://sayapp.company'
+    INSTALLION = 'To ease of access, please install the SAY application.\n%s'
 
