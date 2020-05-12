907a9c5489ad02979cefb43c8a8b40ce8f7fac77
Arashfatahzade <fatahzade@gmail.com>
2020-05-12 17:16:01 +0430

    Fixed oncePurchased

--- say/models/need_model.py
+++ say/models/need_model.py
@@ -381,6 +381,9 @@ def status_event(need, new_status, old_status, initiator):
 
     elif need.type == 1:  # Product
         if new_status == 3:
+            if need.purchase_date is not None:
+                need.oncePurchased = True
+
             need.purchase_date = datetime.utcnow()
 
         elif new_status == 4:
