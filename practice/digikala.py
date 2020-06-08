fe8017d8507528eafc21f2e2762bd075230ac44d
arash <fatahzade@gmail.com>
2020-06-08 15:41:28 +0430

    Fixed crawler not updating done needs

--- practice/digikala.py
+++ practice/digikala.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.digikala.com/product/dkp-1821589/%D9%85%D8%AF%D8%A7%D8%AF-%D8%B1%D9%86%DA%AF%DB%8C-12-%D8%B1%D9%86%DA%AF-%D8%B3%D8%A8%D8%B2-%DA%A9%D8%AF-772'
+url = 'https://www.digikala.com/product/dkp-1788820/%D8%AF%D8%B3%D8%AA%D9%85%D8%A7%D9%84-%DA%A9%D8%A7%D8%BA%D8%B0%DB%8C-200-%D8%A8%D8%B1%DA%AF-%D8%AF%D9%88%D8%B1%D8%AF%D8%A7%D9%86%D9%87-%D9%85%D8%AF%D9%84-ultra-soft-%D8%A8%D8%B3%D8%AA%D9%87-10-%D8%B9%D8%AF%D8%AF%DB%8C'
+
 result = requests.get(url)
 c = result.content
 
