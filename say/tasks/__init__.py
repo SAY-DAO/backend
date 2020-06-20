818f081e0a2e52e2f1abf1a4f679cdf73a702d43
arash <fatahzade@gmail.com>
2020-06-20 17:51:46 +0430

    Finished unpayable products4

--- say/tasks/__init__.py
+++ say/tasks/__init__.py
@@ -7,5 +7,5 @@ from .update_needs import update_needs, update_need, \
 from .report_to_family import report_to_families
 from .report_to_social_worker import report_to_social_workers
 from .subscribe_email import subscribe_email
-
+from .report_unpayables import report_unpayables
 
