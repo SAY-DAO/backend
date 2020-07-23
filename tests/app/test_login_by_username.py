d16e830e278089d50b182a8f634e759ea720aa47
Babak <b.gholampour@gmail.com>
2020-07-23 21:41:29 +0430

    write 3 method with different scenarios for login

--- tests/app/test_login_by_username.py
+++ tests/app/test_login_by_username.py
@@ -7,16 +7,20 @@ class TestLogin(BaseTestClass):
         self.user = self.create_user(self.password)
 
     def test_login_by_username(self):
-
-        res = self.login(
-            username=self.user.userName,
-            password=self.password,
+        res = self.client.post(
+            LOGIN_URL,
+            data={
+                'username': self.user.userName,
+                'password': self.password,
+                'isInstalled': 0,
+            }
         )
         assert res.status_code == 200
         assert res.json['accessToken'] is not None
         assert res.json['refreshToken'] is not None
         assert res.json['user']['id'] is not None
 
+    def test_login_by_username_wrong_password(self):
         # when password is wrong
         res = self.client.post(
             LOGIN_URL,
@@ -28,4 +32,13 @@ class TestLogin(BaseTestClass):
         )
         assert res.status_code == 400
 
-    # TODO: and more...
+    def test_login_by_username_incomplete_parameter(self):
+        # when password is wrong
+        res = self.client.post(
+            LOGIN_URL,
+            data={
+                'username': self.user.userName,
+                'password': self.password,
+            },
+        )
+        assert res.status_code == 400
