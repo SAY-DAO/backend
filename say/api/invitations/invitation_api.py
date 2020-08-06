fa59f44e51d50ca0662fd06cca51bbcf0d339d37
Arash Fatahzade <fatahzade@gmail.com>
2020-08-06 14:19:27 +0430

    Added text to invitation, #2

--- say/api/invitations/invitation_api.py
+++ say/api/invitations/invitation_api.py
@@ -48,34 +48,35 @@ class InvitationAPI(Resource):
             )
 
         invitation = invitation_query.first()
-        if invitation:
-            return invitation
+        if not invitation:
+            invitee_id = None
+            if invitee_username := data.invitee_username:
+                if not (invitee_id := crud.user.id_by_username(invitee_username)):
+                    return {'message': f'user {invitee_username} not exists'}, 701
 
-        invitee_id = None
-        if invitee_username := data.invitee_username:
-            if not (invitee_id := crud.user.id_by_username(invitee_username)):
-                return {'message': f'user {invitee_username} not exists'}, 701
+                try:
+                    roles = crud.user_family.available_roles(
+                        family_id=data.family_id, username=data.invitee_username,
+                    )
+                    if data.role not in roles:
+                        return {'message': f'Only role {roles} is valid'}, 745
 
-            try:
-                roles = crud.user_family.available_roles(
-                    family_id=data.family_id, username=data.invitee_username,
-                )
-                if data.role not in roles:
-                    return {'message': f'Only role {roles} is valid'}, 745
+                except UserAlreadyInFamily:
+                    return {'message': 'user in family'}, 747
+                except NoAvailableRole:
+                    return {'message': 'no available role'}, 744
 
-            except UserAlreadyInFamily:
-                return {'message': 'user in family'}, 747
-            except NoAvailableRole:
-                return {'message': 'no available role'}, 744
+            invitation = Invitation(
+                family_id=data.family_id,
+                inviter_id=inviter_id,
+                invitee_id=invitee_id,
+                role=data.role,
+                text=data.text,
+            )
 
-        invitation = Invitation(
-            family_id=data.family_id,
-            inviter_id=inviter_id,
-            invitee_id=invitee_id,
-            role=data.role,
-        )
+            session.add(invitation)
 
-        session.add(invitation)
+        invitation.text = data.text
         return invitation
 
 
