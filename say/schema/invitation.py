560731f8b9a708a9366d90f8e569cc18f96c7553
Arash Fatahzade <fatahzade@gmail.com>
2020-08-07 21:54:30 +0430

    Added rule validation to invitation

--- say/schema/invitation.py
+++ say/schema/invitation.py
@@ -1,10 +1,18 @@
 from typing import Optional
-from pydantic import constr
+from pydantic import constr, validator
 
 from say.schema.base import CamelModel
+from say.validations import VALID_ROLES
 
 
 class NewInvitationSchema(CamelModel):
     family_id: int
     role: Optional[int] = None
     text: Optional[constr(max_length=128)]
+
+    @validator('role')
+    def name_must_contain_space(cls, role):
+        if role and role not in VALID_ROLES:
+            raise ValueError('invalid role')
+
+        return role
