8af97d60262ea7289571db6e7632b6bc87325a67
Arash Fatahzade <fatahzade@gmail.com>
2020-09-02 12:07:58 +0430

    Reduced report to family time

--- say/celery.py
+++ say/celery.py
@@ -4,7 +4,7 @@ from celery.schedules import crontab
 beat = {
     'report-to-social-workers': {
         'task': 'say.tasks.report_to_social_worker.report_to_social_workers',
-        'schedule': 120,
+        'schedule': crontab(minute=30, hour='2,9'),
     },
     'update-needs': {
         'task': 'say.tasks.update_needs.update_needs',
@@ -12,7 +12,7 @@ beat = {
     },
     'report_to_family': {
         'task': 'say.tasks.report_to_family.report_to_families',
-        'schedule': crontab(minute=30, hour='3'),
+        'schedule': 3600, #crontab(minute=30, hour='3'),
     },
     'report_unpayables': {
         'task': 'say.tasks.report_unpayables.report_unpayables',
