a50f6e015aa98939fe6d661a67327076b53b7eeb
Arash Fatahzade <fatahzade@gmail.com>
2020-09-09 19:16:46 +0430

    CICD #2

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,3 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-echo REACT_APP_API_URI=$API_URL > .env
+set -e
+
+if [ $CI_COMMIT_REF_SLUG == 'release' ]; then
+    echo TAG='release'
+    echo ENVIRONMENT='production'
+    echo DOMAIN='sayapp.company'
+elif [ $CI_COMMIT_REF_SLUG == 'master' ]; then
+    echo TAG='release'
+    echo ENVIRONMENT='staging'
+    echo DOMAIN='s.sayapp.company'
+else
+    echo TAG='develop'
+    echo ENVIRONMENT='development'
+    echo DOMAIN='d.sayapp.company'
+fi
