2bedec6f1b88c977b5a1b5f9520a4bb4f06f5ae6
Arash Fatahzade <fatahzade@gmail.com>
2020-09-10 03:58:19 +0430

    Added flower config

--- flowerconfig.py
+++ flowerconfig.py
@@ -1,12 +1,4 @@
-import os
-
-def get_secret(secret_name, default=None):
-    try:
-        with open(f'/run/secrets/{secret_name}', 'r') as secret_file:
-            return secret_file.read().strip()
-    except IOError:
-        env_name = secret_name.upper().replace('-', '_')
-        return os.environ.get(env_name, default)
+from say.helpers import get_secret
 
 
 USERNAME = 'dev'
