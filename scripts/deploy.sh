edc177918fb37ca6eb7644882125ce7fadcc19c4
Arash Fatahzade <fatahzade@gmail.com>
2020-09-11 23:00:19 +0430

    Fixing zero downtime

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,16 @@
 
 set -e
 
+if [[ $ENVIRONMENT == 'prod' ]]
+then
+    OVERRIDE_IF_PROD='-f docker-compose-prod.yml'
+else
+    OVERRIDE_IF_PROD=''
+fi
+
 docker-compose \
     -f docker-compose.yml \
+    $OVERRIDE_IF_PROD \
     config > docker-stack.yml
 
 docker stack deploy \
