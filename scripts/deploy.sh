fdc105d927378c665c4cf1e46b5431bb8a1ee224
Arash Fatahzade <fatahzade@gmail.com>
2020-09-11 23:50:48 +0430

    Reset db ports

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-if [[ $ENVIRONMENT == 'prod' ]]
-then
-    OVERRIDE_IF_PROD='-f docker-compose-prod.yml'
-else
-    OVERRIDE_IF_PROD=''
-fi
-
 docker-compose \
     -f docker-compose.yml \
-    $OVERRIDE_IF_PROD \
     config > docker-stack.yml
 
 docker stack deploy \
