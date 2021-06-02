fe3022f27c7ecbe93a7ca206cdc804da2098b6de
Arash Fatahzade <fatahzade@gmail.com>
2021-06-02 17:38:12 +0430

    Increase idpay timeout

--- say/payment/idpay.py
+++ say/payment/idpay.py
@@ -11,7 +11,7 @@ class IDPay:
     API_URL = "https://api.idpay.ir/v1.1/payment"
     MIN_AMOUNT = 100  # TOMAN
     TRY_COUNT = 3
-    TIMEOUT = 30
+    TIMEOUT = 59
 
     RESPONSES = {
         1: "پرداخت انجام نشده است",
