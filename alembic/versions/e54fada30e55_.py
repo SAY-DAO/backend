b063c2582776ed56af414b17759c6e722ca252ca
Arash Fatahzade <fatahzade@gmail.com>
2021-07-27 03:09:00 +0430

    Merged migrations

---
+++ alembic/versions/e54fada30e55_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e54fada30e55
+Revises: 67a28a3f9f9f, 0ebe70e88c88
+Create Date: 2021-07-27 03:08:33.767009
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e54fada30e55'
+down_revision = ('67a28a3f9f9f', '0ebe70e88c88')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
