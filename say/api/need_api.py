8e8b084151c2df205ce79039b20eb1eb9bf71819
Arash Fatahzade <fatahzade@gmail.com>
2021-08-23 11:55:26 +0430

    fix missing import, https://sentry.say.company/organizations/sentry/issues/1127/?environment=prod&project=4&query=is%3Aunresolved

--- say/api/need_api.py
+++ say/api/need_api.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 
 import ujson
 from flasgger import swag_from
+from flask import jsonify
 from flask import request
 from flask_restful import Resource
 from sqlalchemy import or_
