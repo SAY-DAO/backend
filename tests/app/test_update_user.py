7c93f349103c048505248cc54b3d68be76767bdf
Arash Fatahzade <fatahzade@gmail.com>
2021-11-20 20:32:34 +0330

    Uncommented update user test

--- tests/app/test_update_user.py
+++ tests/app/test_update_user.py
@@ -15,41 +15,41 @@ class TestUpdateUser(BaseTestClass):
         self.pw = '123456'
         self.user = self._create_random_user()
 
-    # # TODO: Write test for errors
-    # @pytest.mark.parametrize(
-    #     'field,value,expected',
-    #     [
-    #         ('receiveEmail', False, 200),
-    #         ('firstName', 'arash', 200),
-    #         ('lastName', 'FZ', 200),
-    #         ('country', 'ir', 200),
-    #         ('city', 1, 200),
-    #         ('postal_address', '1234567890', 200),
-    #         ('postal_code', '1234567890', 200),
-    #         ('birthPlace', 1, 200),
-    #         ('locale', 'en', 200),
-    #         ('birthDate', '1999-09-09', 200),
-    #         ('birthDate', '1999-29-59', 400),
-    #         ('gender', 'male', 200),
-    #     ],
-    # )
-    # def test_user_update_me(self, field, value, expected):
-    #     self.login(self.user)
+    # TODO: Write test for errors
+    @pytest.mark.parametrize(
+        'field,value,expected',
+        [
+            ('receiveEmail', False, 200),
+            ('firstName', 'arash', 200),
+            ('lastName', 'FZ', 200),
+            ('country', 'ir', 200),
+            ('city', 1, 200),
+            ('postal_address', '1234567890', 200),
+            ('postal_code', '1234567890', 200),
+            ('birthPlace', 1, 200),
+            ('locale', 'en', 200),
+            ('birthDate', '1999-09-09', 200),
+            ('birthDate', '1999-29-59', 400),
+            ('gender', 'male', 200),
+        ],
+    )
+    def test_user_update_me(self, field, value, expected):
+        self.login(self.user)
 
-    #     res = self.client.patch(
-    #         USER_UPDATE_URL % 'me',
-    #         data={
-    #             field: value,
-    #         },
-    #     )
-    #     assert res.status_code == expected
-    #     if expected == 200:
-    #         expected_value = res.json.get(field)
+        res = self.client.patch(
+            USER_UPDATE_URL % 'me',
+            data={
+                field: value,
+            },
+        )
+        assert res.status_code == expected
+        if expected == 200:
+            expected_value = res.json.get(field)
 
-    #         if field == 'birthDate':
-    #             expected_value = jsonify(datetime.strptime(value, '%Y-%m-%d')).json
+            if field == 'birthDate':
+                expected_value = jsonify(datetime.strptime(value, '%Y-%m-%d')).json
 
-    #         assert res.json.get(field) == expected_value
+            assert res.json.get(field) == expected_value
 
     def test_user_update_me_avatar(self):
         self.login(self.user)
