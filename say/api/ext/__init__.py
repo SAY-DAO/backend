0cc039e0d6d2e570a6e9a407d4d86d2fdf8b4c35
Arash Fatahzade <fatahzade@gmail.com>
2021-11-25 03:40:01 +0330

    Fixed limiter ip detection, 2

--- say/api/ext/__init__.py
+++ say/api/ext/__init__.py
@@ -4,7 +4,6 @@ import redis
 from flask import request
 from flask_jwt_extended import JWTManager
 from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 from flask_mail import Mail
 from flask_restful import Api
 from mailerlite import MailerLiteApi
@@ -12,21 +11,6 @@ from mailerlite import MailerLiteApi
 from say.config import configs
 from say.payment import IDPay
 from say.sms import MeliPayamak
-
-
-api = Api()
-jwt = JWTManager()
-idpay = IDPay(configs.IDPAY_API_KEY, configs.SANDBOX)
-sms_provider = MeliPayamak(
-    configs.MELI_PAYAMAK_USERNAME,
-    configs.MELI_PAYAMAK_PASSWORD,
-    configs.MELI_PAYAMAK_FROM,
-)
-mailerlite = MailerLiteApi(configs.MAILERLITE_API_KEY)
-redis_client = redis.Redis(configs.REDIS_HOST, configs.REDIS_PORT)
-mail = Mail()
-limiter = Limiter(key_func=get_remote_address)
-logger = getLogger('main')
 
 
 CLOUDFLARE_IP_HEADER_KEY = 'Cf-Connecting-Ip'
@@ -44,6 +28,21 @@ def get_remote_address():
         or request.remote_addr
         or '127.0.0.1'
     )
+
+
+api = Api()
+jwt = JWTManager()
+idpay = IDPay(configs.IDPAY_API_KEY, configs.SANDBOX)
+sms_provider = MeliPayamak(
+    configs.MELI_PAYAMAK_USERNAME,
+    configs.MELI_PAYAMAK_PASSWORD,
+    configs.MELI_PAYAMAK_FROM,
+)
+mailerlite = MailerLiteApi(configs.MAILERLITE_API_KEY)
+redis_client = redis.Redis(configs.REDIS_HOST, configs.REDIS_PORT)
+mail = Mail()
+limiter = Limiter(key_func=get_remote_address)
+logger = getLogger('main')
 
 
 def setup_healthz(app):
