d2192b152fff345a0b81007876e67ad7feb15dbe
Ehsan G <60781475+ehsan-g@users.noreply.github.com>
2023-09-01 18:08:54 +0330

    Update fa.py

--- say/content/fa.py
+++ say/content/fa.py
@@ -1,4 +1,4 @@
 class ContentFA:
-    CONFIRM_PHONE = '%s کد تایید شش رقمی شما در SAY است.لغو11 '
-    RESET_PASSWORD = 'برای تغییر گذرواژ روی لینک زیر کلیک کنید.\n%s'
-    INSTALLION = 'برای استفاده آسان از SAY آن را نصب کنید.\n%s'
+    CONFIRM_PHONE = "%s کد تایید شش رقمی شما در SAY است.لغو11 "
+    RESET_PASSWORD = "برای تغییر گذرواژ روی لینک زیر کلیک کنید. \n%s لغو11"
+    INSTALLION = "برای استفاده آسان از SAY آن را نصب کنید.\n%s"
