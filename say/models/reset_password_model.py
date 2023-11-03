8f748a32bd51b2928b631bc6a1aa260c4a13618f
Gbuntu <ehsan@say.company>
2023-11-03 22:27:51 +0330

    reset pass url

--- say/models/reset_password_model.py
+++ say/models/reset_password_model.py
@@ -42,7 +42,7 @@ class ResetPassword(base):
     @property
     def link(self):
         return urljoin(
-            'https://dapp.saydao.org', auth/reset-password + f'?token={self.token}'
+            'https://dapp.saydao.org', 'auth/reset-password' + f'?token={self.token}'
         )
 
     def send_email(self, language):
