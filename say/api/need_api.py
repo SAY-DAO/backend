03f93afae96e70ba64b6d330029ad2f9c96f02e3
Ehsan G <60781475+ehsan-g@users.noreply.github.com>
2024-03-03 10:56:01 +0330

    Update need_api.py
    'bug fix: needs with no icons'

--- say/api/need_api.py
+++ say/api/need_api.py
@@ -602,7 +602,8 @@ class AddNeed(Resource):
 
 #         image_path = 'wrong path'
         image_path = request.form.get('imageUrl', '')
-
+         if not image_path:
+            return {'message': 'error: we need the need icon!'}, 400
         image_url = image_path
 
         category = request.form.get('category')
