ecc6ac9a999efa07542776ba98639b0419b7ef04
G <ehsan@say.company>
2026-02-07 22:48:40 +0330

    schedule update

--- say/celery.py
+++ say/celery.py
@@ -23,7 +23,7 @@ beat = {
     },
     'update-needs': {
         'task': 'say.tasks.update_needs.update_needs',
-        'schedule': crontab(minute=30, hour='6'),
+        'schedule': crontab(minute=30, hour=6, day_of_week='*/2'),
     },
     'report_to_family': {
         'task': 'say.tasks.report_to_family.report_to_families',
